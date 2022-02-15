10b28cba9b1cdabc82973b874522ec13de95d423
olarno <43755813+olarno@users.noreply.github.com>
2022-02-15 15:34:15 +0000

    update with extract fies

--- crypt.php
+++ crypt.php
@@ -7,7 +7,7 @@ $data = 'plaintext data goes here';
 openssl_public_encrypt($data, $encrypted, openssl_pkey_get_public(file_get_contents('keys/public.pem')));
 
 echo 'Data encrypted :'.$encrypted; 
-
+file_put_contents('encrypt/data', $encrypted);
 // Decrypt the data using the private key and store the results in $decrypted
 openssl_private_decrypt($encrypted, $decrypted, openssl_get_privatekey(file_get_contents('keys/private.pem')));
 
